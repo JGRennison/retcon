58d1494f02b87e0676b172491b24e4c3c48925a7
Jonathan G Rennison <j.g.rennison@gmail.com>
2013-10-17 21:34:29 +0100

    Update all license headers. Add a few missing newlines.

--- src/cfg.h
+++ src/cfg.h
@@ -2,23 +2,20 @@
 //
 //  WEBSITE: http://retcon.sourceforge.net
 //
-//  NOTE: This software is licensed under the GPL. See: COPYING-GPL.txt
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version. See: COPYING-GPL.txt
 //
 //  This program  is distributed in the  hope that it will  be useful, but
 //  WITHOUT   ANY  WARRANTY;   without  even   the  implied   warranty  of
 //  MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR PURPOSE.   See  the GNU
 //  General Public License for more details.
 //
-//  Jonathan Rennison (or anybody else) is in no way responsible, or liable
-//  for this program or its use in relation to users, 3rd parties or to any
-//  persons in any way whatsoever.
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see <http://www.gnu.org/licenses/>.
 //
-//  You  should have  received a  copy of  the GNU  General Public
-//  License along  with this program; if  not, write to  the Free Software
-//  Foundation, Inc.,  59 Temple Place,  Suite 330, Boston,  MA 02111-1307
-//  USA
-//
-//  2012 - j.g.rennison@gmail.com
+//  2012 - Jonathan G Rennison <j.g.rennison@gmail.com>
 //==========================================================================
 
 #include <sqlite3.h>
