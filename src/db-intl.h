69ca8cb6683a1becbcda7c514df4903cf9cba948
Jonathan G Rennison <j.g.rennison@gmail.com>
2014-04-13 15:23:59 +0100

    Fix character encoding mismatch in DB utility function error logging.

--- src/db-intl.h
+++ src/db-intl.h
@@ -187,7 +187,7 @@ template <typename E> void DBDoErr(E errspec, sqlite3 *adb, sqlite3_stmt *stmt, 
 }
 
 template <> void DBDoErr<std::string>(std::string errspec, sqlite3 *adb, sqlite3_stmt *stmt, int res) {
-	LogMsgFormat(LOGT::DBERR, wxT("%s got error: %d (%s)"), errspec.c_str(), res, wxstrstd(sqlite3_errmsg(adb)).c_str());
+	LogMsgFormat(LOGT::DBERR, wxT("%s got error: %d (%s)"), wxstrstd(errspec).c_str(), res, wxstrstd(sqlite3_errmsg(adb)).c_str());
 }
 
 template <> void DBDoErr<const char *>(const char *errspec, sqlite3 *adb, sqlite3_stmt *stmt, int res) {
