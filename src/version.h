4ee7c8aa65fdbee53a113a6b140f4e18b9507228
Jonathan G Rennison <j.g.rennison@gmail.com>
2014-06-21 22:39:01 +0100

    Bump version and changelog to v0.0.9.

--- src/version.h
+++ src/version.h
@@ -21,7 +21,7 @@
 #include "univdefs.h"
 #include <wx/string.h>
 
-#define RETCON_VERSION_STR "0.0.8"
+#define RETCON_VERSION_STR "0.0.9"
 
 extern const wxString appname;
 extern const wxString appversionname;
